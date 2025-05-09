6aed672d8d3ee09ede75df7c6ae6830e62cb8003
Chatarina Gifta <giftadiyana5@gmail.com>
2025-05-09 22:27:13 +0700

    Update projectAlpro.cpp

--- projectAlpro.cpp
+++ projectAlpro.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 const int max_Menu = 100;
-const int max_toko = 5;
 string carinama; 
 char awal;
 int pilih, pilih1, pilih2;
@@ -59,36 +59,44 @@ Menu daftarMenu[max_Menu] =
         {"Wedang Uwuh", 500000, "Nusantara"},
         {"Es Dawet", 400000, "Nusantara"},
 
-        {"Ayam Geprek", 150000, "Gepriks"},
+        {"Ayam Geprek lv 0", 150000, "Gepriks"},
+        {"Ayam Geprek lv 1", 160000, "Gepriks"},
+        {"Ayam Geprek lv 2", 170000, "Gepriks"},
+        {"Ayam Geprek lv 3", 180000, "Gepriks"},
+        {"Ayam Geprek lv 4", 190000, "Gepriks"},
         {"Es Teh", 300000, "Gepriks"},
         {"Es Jeruk", 200000, "Gepriks"},
         {"Es Cokelat", 400000, "Gepriks"}
 };
 
-void tampilkanMenu(const string& namaWarung) {
-    
-        cout << "======= MENU " << namaWarung << "=======" << endl;
-        cout << left << setw(20) << "Makanan " << " Harga " << endl;
+string toLowerCase(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
 
+void tampilkanMenu(const string& namaWarung)
+{
+        cout << "======= Menu " << namaWarung << " =======" << endl;
+        cout << left << setw(20) << "Makanan " << "   Harga " << endl;
         for (int i = 0; i < 5; i++) {
             cout << left << i + 1 << ". "<< setw(20) << daftarMenu[i].nama
                  << setw(5) << daftarMenu[i].harga << "\n";
         }
-
+        cout << left << "6. " << setw(20) << "Tidak pesan makanan\n";
         cout << "\nMinuman " << endl;
 
-        for (int i = 5; i < 8; i++) {
+        for (int i = 6; i < 8; i++) {
             cout << left << i + 1 << ". " << setw(20) << daftarMenu[i].nama
                  << setw(5) << daftarMenu[i].harga << "\n";
         }
-
         cout << left << "9. " << setw(20) << "Kembali ke menu awal\n";
-    
+        cout << left << "10. " << setw(20) << "Tidak pesan minuman\n";
 }
 
-void cariMenuDiWarung(const string& namaWarung) {
+void cariMenuDiWarung(const string& namaWarung)
+{
     bool found = false;
-    cout << "Masukkan nama menu yang dicari: ";
+    cout << "Masukkan nama menu yang dicari : ";
     cin.ignore();
     getline(cin, carinama);
 
@@ -101,7 +109,7 @@ void cariMenuDiWarung(const string& namaWarung) {
             found = true;
         }
     }
-    cout << "Kembali ke menu awal (y/n) ? : "; cin >> awal;
+    cout << "Kembali ke menu awal (y/n)? : "; cin >> awal;
     if (awal == 'y' || awal == 'Y')
         if (namaWarung == "Makyus")
             makyus();
@@ -117,15 +125,22 @@ void cariMenuDiWarung(const string& namaWarung) {
         cout << "Menu tidak ditemukan di warung " << namaWarung << ".\n";
 }
 
+void sorting()
+{   
+
+    cout << "1. Ascending\n";
+    cout << "2. Descending\n";
+}
+
 void makyus ()
 {   
     system("cls");
     do {
-        cout << "* Warung Makyus *\n1. Lihat menu\n2. Cari menu\n3. Kembali ke menu utama\nPilih : ";
+        cout << "* Warung Makyus *\n1. Lihat menu\n2. Cari menu\n3. Sorting\n4. Kembali ke menu utama\nPilih : ";
         cin >> pilih;
         switch (pilih) {
             case 1:
-                while (pilih1 > 9 || pilih1 < 1){
+                while (pilih1 > 10 || pilih1 < 1){
                 tampilkanMenu("Makyus");
                 cout << "Pilih : "; cin >> pilih1;
                 }
@@ -134,8 +149,12 @@ void makyus ()
             case 2:
                 cariMenuDiWarung("Makyus");
                 break;
+            
+            case 3 :
+                sorting();
+                break;
 
-            case 3:
+            case 4:
                 menu();
                 break;
 
@@ -143,7 +162,7 @@ void makyus ()
                 cout << " Pilihan tidak valid!\n";
                 break;
         }
-    } while (pilih > 3 || pilih < 1 || pilih1 == 9);
+    } while (pilih > 4 || pilih < 1 || pilih1 == 9);
 }
 
 void Legenda ()
@@ -154,12 +173,13 @@ void Legenda ()
     cout << "* Warung Legenda *\n";
     cout << "1. Lihat menu\n";
     cout << "2. Cari menu\n";
-    cout << "3. Kembali ke menu utama\n";
+    cout << "3. Sorting\n";
+    cout << "4. Kembali ke menu utama\n";
     cout << "Pilih : "; cin >> pilih;
     switch (pilih)
     {
         case 1 : 
-            while (pilih1 > 9 || pilih1 < 1){
+            while (pilih1 > 10 || pilih1 < 1){
                 tampilkanMenu("Legenda");
                 cout << "Pilih : "; cin >> pilih1;
             }
@@ -168,8 +188,11 @@ void Legenda ()
         case 2 :
             cariMenuDiWarung("Legenda");
             break;
-
         case 3 :
+            sorting();
+            break;
+
+        case 4 :
             menu();
             break;
 
@@ -187,11 +210,12 @@ void FoodKuy ()
     cout << "* Warung FoodKuy *\n";
     cout << "1. Lihat menu\n";
     cout << "2. Cari menu\n";
-    cout << "3. Exit\n";
+    cout << "3. Sorting\n";
+    cout << "4. Kembali ke menu utama\n";
     cout << "Pilih : "; cin >> pilih;
     switch ( pilih ) {
         case 1 :
-        while (pilih1 > 9 || pilih1 < 1){
+        while (pilih1 > 10 || pilih1 < 1){
             tampilkanMenu("Foodkuy");
             cout << "Pilih : "; cin >> pilih1;
         }
@@ -202,7 +226,11 @@ void FoodKuy ()
         break;
     
     case 3 :
-        exit(0);
+        sorting();
+        break;
+
+    case 4 :
+        menu();
         break;
 
     default :
@@ -220,12 +248,13 @@ void Nusantara ()
     cout << "* Warung Nusantara *\n";
     cout << "1. Lihat menu\n";
     cout << "2. Cari menu\n";
-    cout << "3. Exit\n";
+    cout << "3. Sorting\n";
+    cout << "4. Kembali ke menu utama\n";
     cout << "Pilih : "; cin >> pilih;
     switch (pilih)
     {
         case 1 :
-        while (pilih1 > 9 || pilih < 1) {
+        while (pilih1 > 9 || pilih1 < 1) {
                 tampilkanMenu("Nusantara");
                 cout << "Pilih : "; cin >> pilih1;
             }
@@ -234,9 +263,13 @@ void Nusantara ()
         case 2 :
             cariMenuDiWarung("Nusantara");
             break;
-
+        
         case 3 :
-            exit(0);
+            sorting();
+            break;
+
+        case 4 :
+            menu();
             break;
 
         default:
@@ -254,7 +287,8 @@ void Gepriks()
         cout << "* Warung Gepriks *\n";
         cout << "1. Lihat menu\n";
         cout << "2. Cari menu\n";
-        cout << "3. Exit\n";
+        cout << "3. Sorting\n";
+        cout << "4. Kembali ke menu utama\n";
         cout << "Pilih : "; cin >> pilih;
         switch (pilih)
         {
@@ -276,7 +310,7 @@ void Gepriks()
     } while (pilih > 3 || pilih < 1);
 }
 
-void cariMenu ()
+void cariMenu (const string& namaWarung)
 {
     system ("cls");
     bool found = false;
@@ -284,18 +318,19 @@ void cariMenu ()
     cin.ignore();
     getline(cin, carinama);
     cout << "\nMenu yang ditemukan : \n";
-    for (int i = 0; i < 40; i++)
+    for (int i = 0; i < max_Menu; i++)
     {
-        if (daftarMenu[i].nama == carinama)
+        if (toLowerCase(daftarMenu[i].namaWarung) == toLowerCase(namaWarung) &&
+            toLowerCase(daftarMenu[i].nama) == toLowerCase(carinama))
         {
-            cout << "\nMenu ditemukan di " << daftarMenu[i].namaWarung << ": "
+            cout << "Menu ditemukan di " << daftarMenu[i].namaWarung << ": "
                  << daftarMenu[i].nama << " - Rp" << daftarMenu[i].harga << endl;
             found = true;
         }
     }
 
     if (!found)
-        cout << "Menu tidak ditemukan.\n";
+        cout << "Menu tidak ditemukan di warung \n" << namaWarung << ".";
 }
 
 void menu ()
@@ -303,7 +338,7 @@ void menu ()
    do {
     system("cls");
     cout << " * Selamat datang di Kuliner Street Food ! * " << endl;
-    cout << " 1. Makyus \n 2. Jajanan Legenda \n 3. Street Food Kuy! \n 4. Nusantara\n";
+    cout << " 1. Makyus \n 2. Legenda \n 3. FoodKuy! \n 4. Nusantara\n";
     cout << " 5. Gepriks\n 6. Keluar\n";
     cout << " Pilih Warung : ";
     cin >> pilih;
